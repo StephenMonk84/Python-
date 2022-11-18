8f32e6211d33d060b923854e5bca05d58f791e17
Stephen Monk <monk.stephen@outlook.com>
2022-11-17 20:53:08 -0800

    First Python Script with a class

---
+++ Class_Cat_testing.py
@@ -0,0 +1,13 @@
+class Cat:
+    colour = ""
+    tail = True
+    legs = 0
+    def cat_says():
+        print("Meow")
+
+bubbs = Cat
+bubbs.colour = "Black"
+bubbs.legs = 4
+bubbs.cat_says()
+print(bubbs.colour)
+print('Bubbs has {} legs'.format(bubbs.legs))
